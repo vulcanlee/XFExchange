3966ba4895b8b628af5afd8ba58efaf973920528
李進興 Vulcan <vulcan@miniasp.com>
2016-12-03 18:15:25 +0800

    How to implement a Radial Menu

---
+++ XFRadialMenu/XFRadialMenu/XFRadialMenu/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Unity;
+using XFRadialMenu.Views;
+
+namespace XFRadialMenu
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+
+            NavigationService.NavigateAsync("MainPage?title=Hello%20from%20Xamarin.Forms");
+        }
+
+        protected override void RegisterTypes()
+        {
+            Container.RegisterTypeForNavigation<MainPage>();
+        }
+    }
+}
---
+++ XFRadialMenu/XFRadialMenu/XFRadialMenu/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,59 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace XFRadialMenu.ViewModels
+{
+    public class MainPageViewModel : BindableBase, INavigationAware
+    {
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        #region ShowMenu
+        private bool _ShowMenu=false;
+        /// <summary>
+        /// ShowMenu
+        /// </summary>
+        public bool ShowMenu
+        {
+            get { return this._ShowMenu; }
+            set { this.SetProperty(ref this._ShowMenu, value); }
+        }
+        #endregion
+
+        public DelegateCommand MenuCommand { get; set; }
+
+        public DelegateCommand<string> SubMenuCommand { get; set; }
+
+        public MainPageViewModel()
+        {
+            MenuCommand = new DelegateCommand(() =>
+            {
+                ShowMenu = !ShowMenu;
+            });
+            SubMenuCommand = new DelegateCommand<string>(x =>
+            {
+                Title = $"Your press {x}";
+                ShowMenu = false;
+            });
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+            if (parameters.ContainsKey("title"))
+                Title = (string)parameters["title"] + " and Prism";
+        }
+    }
+}
