21e1ddcc272c9c6b5e3f92ccd6e6719eb6d2d829
李進興 Vulcan <vulcan@miniasp.com>
2017-01-04 00:16:25 +0800

    MQTT

---
+++ XFMqtt/XFMqtt/XFMqtt.Droid/MainActivity.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Prism.Unity;
+using Microsoft.Practices.Unity;
+using uPLibrary.Networking.M2Mqtt;
+using System.Net;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using System.Text;
+using Prism.Events;
+using Newtonsoft.Json;
+
+namespace XFMqtt.Droid
+{
+    [Activity(Label = "XFMqtt", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        string MQTT_BROKER_ADDRESS = "192.168.1.104";
+        string strValue = "Hello";
+        IEventAggregator fooEventAggregator;
+        MqttClient client;
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.tabs;
+            ToolbarResource = Resource.Layout.toolbar;
+
+            base.OnCreate(bundle);
+
+            MtqqInit();
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            var fooApp = new App(new AndroidInitializer());
+
+
+            fooEventAggregator = fooApp.Container.Resolve<IEventAggregator>();
+
+            fooEventAggregator.GetEvent<SendMessageEvent>().Subscribe(x =>
+            {
+                var fooValue = JsonConvert.SerializeObject(x);
+                client.Publish("/home/temperature", Encoding.UTF8.GetBytes(fooValue), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
+            });
+
+            LoadApplication(fooApp);
+        }
+
+        private void MtqqInit()
+        {
+            // create client instance 
+            client = new MqttClient(MQTT_BROKER_ADDRESS);
+
+            // register to message received 
+            client.MqttMsgPublishReceived += Client_MqttMsgPublishReceived;
+
+            string clientId = Guid.NewGuid().ToString();
+            client.Connect(clientId);
+
+            // subscribe to the topic "/home/temperature" with QoS 2 
+            client.Subscribe(new string[] { "/home/temperature" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+
+        }
+
+        private void Client_MqttMsgPublishReceived(object sender, uPLibrary.Networking.M2Mqtt.Messages.MqttMsgPublishEventArgs e)
+        {
+            var fooValue = Encoding.UTF8.GetString(e.Message);
+            var fooMessageType = JsonConvert.DeserializeObject<MessageType>(fooValue);
+            fooEventAggregator.GetEvent<ReceiveMessageEvent>().Publish(fooMessageType);
+        }
+
+        //static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        //{
+        //    // handle message received 
+        //}
+    }
+
+    public class AndroidInitializer : IPlatformInitializer
+    {
+        public void RegisterTypes(IUnityContainer container)
+        {
+
+        }
+    }
+}
+
---
+++ XFMqtt/XFMqtt/XFMqtt/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,98 @@
+﻿using Prism.Commands;
+using Prism.Events;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace XFMqtt.ViewModels
+{
+    public class MainPageViewModel : BindableBase, INavigationAware
+    {
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+
+        #region SendingMessage
+        private string _SendingMessage="";
+        /// <summary>
+        /// SendingMessage
+        /// </summary>
+        public string SendingMessage
+        {
+            get { return this._SendingMessage; }
+            set { this.SetProperty(ref this._SendingMessage, value); }
+        }
+        #endregion
+
+
+        #region UserID
+        private string _UserID = "";
+        /// <summary>
+        /// UserID
+        /// </summary>
+        public string UserID
+        {
+            get { return this._UserID; }
+            set { this.SetProperty(ref this._UserID, value); }
+        }
+        #endregion
+
+
+        #region SendingUserID
+        private string _SendingUserID = "";
+        /// <summary>
+        /// SendingUserID
+        /// </summary>
+        public string SendingUserID
+        {
+            get { return this._SendingUserID; }
+            set { this.SetProperty(ref this._SendingUserID, value); }
+        }
+        #endregion
+
+
+        public DelegateCommand SendingCommand { get; set; }
+
+        private readonly IEventAggregator _eventAggregator;
+
+        public MainPageViewModel(IEventAggregator eventAggregator)
+        {
+
+            _eventAggregator = eventAggregator;
+
+            _eventAggregator.GetEvent<ReceiveMessageEvent>().Subscribe(x =>
+            {
+                if(x.To == UserID)
+                {
+                    Title = $"接收到的訊息:{x.Content}";
+                }
+            });
+            
+            SendingCommand = new DelegateCommand(() =>
+            {
+                _eventAggregator.GetEvent<SendMessageEvent>().Publish(new MessageType
+                {
+                    Content = SendingMessage,
+                    To = SendingUserID
+                });
+            });
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+            if (parameters.ContainsKey("title"))
+                Title = (string)parameters["title"] + " and Prism";
+        }
+    }
+}
